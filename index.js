9acc1713d53bdc493c7f248896cdbee190edaf4c
zy254263 <zy254263@gmail.com>
2019-11-04 06:34:43 +0000

    BOT内容変更

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // モジュールのインポート
 const server = require('express')();
 const line = require('@line/bot-sdk'); // Messaging APIのSDKをインポート
+const dialogflow = require('dialogflow');
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -15,6 +16,15 @@ const lineConfig = {
 server.listen(process.env.PORT || 3000);
 
 const bot = new line.Client(lineConfig);
+
+// Dialogflowのクライアントインスタンスを作成
+const sessionClient = new dialogflow.SessionsClient({
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  },
+});
 
 // -----------------------------------------------------------------------------
 // ルーター設定
@@ -30,12 +40,30 @@ server.post('/bot/webhook', line.middleware(lineConfig), (req, res, next) => {
   req.body.events.forEach((event) => {
     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
     if (event.type === 'message' && event.message.type === 'text') {
-      // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-      // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-      eventsProcessed.push(bot.replyMessage(event.replyToken, {
-        type: 'text',
-        text: 'NHKをぶっ壊す！',
-      }));
+      eventsProcessed.push(
+        sessionClient.detectIntent({
+          session: sessionClient.sessionPath(process.env.GOOGLE_PROJECT_ID, event.source.userId),
+          queryInput: {
+            text: {
+              text: event.message.text,
+              languageCode: 'ja',
+            },
+          },
+        }).then((responses) => {
+          if (responses[0].queryResult && responses[0].queryResult.action === 'searchRanking') {
+            let messageText;
+            if (responses[0].queryResult.parameters.fields.menu.stringValue) {
+              messageText = `毎度！${responses[0].queryResult.parameters.fields.menu.stringValue}ね。どちらにお届けしましょ？`;
+            } else {
+              messageText = '毎度！ご注文は？';
+            }
+            return bot.replyMessage(event.replyToken, {
+              type: 'text',
+              text: messageText,
+            });
+          }
+        }),
+      );
     }
   });
 
